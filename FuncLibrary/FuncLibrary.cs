d1428b8b97cffa8fe0ec386acf2d79e19cf0d5dd
Daveltrix <Davltrixprogramacion@gmail.com>
2023-04-05 20:16:31 +0200

    Libreria de funciones agregada y lectura de los jugadores a traves de un archivo .ied

---
+++ FuncLibrary/FuncLibrary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FuncLibrary
+{
+    public class FunctionClass
+    {
+        public static void Read_Xml(String path_ied)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(path_ied);
+
+            XmlNodeList playerNodes = xmlDoc.SelectNodes("//player");
+            foreach (XmlNode playerNode in playerNodes)
+            {
+                String nombre = playerNode.Attributes["name"].Value;
+                String id = playerNode.Attributes["id"].Value;
+                String dinero = playerNode.Attributes["dinero"].Value;
+                String figura = playerNode.Attributes["figura"].Value;
+
+                Console.WriteLine($"nombre={nombre} id={id} dinero={dinero} figura={figura}");
+            }
+        }
+    }
+}
--- Monopoly/Program.cs
+++ Monopoly/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Xml;
+using FuncLibrary;
 
 namespace MiPrograma
 {
@@ -8,10 +10,13 @@ namespace MiPrograma
         {
             Console.WriteLine("COMIENZA LA PARTIDA MAS DIVERTIDA DE TU VIDA: EL MONOPOLY");
 
-
+            String path_ied = "Game.ied";
+            
+            FunctionClass.Read_Xml(path_ied);
 
 
             Console.WriteLine("Authors: PASCUAL Y JAIME");
+            Console.ReadKey();
         }
     }
 }
