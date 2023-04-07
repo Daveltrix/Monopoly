936cc647f2d1a76e98956746a0aa92756a160140
Daveltrix <Davltrixprogramacion@gmail.com>
2023-04-07 13:33:05 +0200

    Clase para funciones varias. En este caso para lanzar el dado aleatoriamente

---
+++ Monopoly/FVariousFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public class FVariousFunctions
+    {
+        public FVariousFunctions() { }
+
+        public static Int32 LanzarDado()
+        {
+            Random rnd = new Random();
+            int RandomNumber = rnd.Next(1, 7);
+            return RandomNumber;
+        }
+    }
+}
