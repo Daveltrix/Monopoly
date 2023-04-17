0c8a4836890b6dc06813b3669e42732647a0d701
Daveltrix <Davltrixprogramacion@gmail.com>
2023-04-17 19:15:33 +0200

    Change name of class and add a property

---
+++ Monopoly/CPokemons.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    public class CPokemons
+    {
+        public String Name { get; set; }
+        public Int32 Value { get; set; }
+        public String Color { get; set; }
+        public Boolean Turn { get; set; }
+        public String Trainer { get; set; }
+
+        public CPokemons() { }
+
+        public CPokemons (string name, int value, string color, bool turn)
+        {
+            Name = name;
+            Value = value;
+            Color = color;
+            Turn = turn;
+        }
+    }
+}
